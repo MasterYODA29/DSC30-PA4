5db6070b3f6823222f796a9f8e8368296c1d9d98
MasterYODA29 <m2ren@ucsd.edu>
2024-08-31 23:37:39 -0700

    second commit

--- BSTreeTester.java
+++ BSTreeTester.java
@@ -1,5 +1,5 @@
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
